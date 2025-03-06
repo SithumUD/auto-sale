9f69042c4e83d7a663964333ecd31d5829a21103
SITHUM UDAYANGA <sithumudayangaofficial@gmail.com>
2025-03-06 13:05:09 +0530

    initial commit

---
+++ auto-sale-backend/src/main/java/com/sithum/auto_sale/AutoSaleApplication.java
@@ -0,0 +1,13 @@
+package com.sithum.auto_sale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoSaleApplication.class, args);
+	}
+
+}
---
+++ auto-sale-backend/src/test/java/com/sithum/auto_sale/AutoSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sithum.auto_sale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoSaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
